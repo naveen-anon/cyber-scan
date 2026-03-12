385ff8d0d3adda71007c863194a00a277dd7e9be
Naveen khatri <naveen1khatri@gmail.com>
2026-03-12 18:00:34 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
+function show(data){
+
+document.getElementById("results").textContent =
+JSON.stringify(data,null,2)
+
+}
+
 function ipSearch(){
 
 let ip=document.getElementById("ip").value
@@ -10,9 +17,38 @@ headers:{
 body:"ip="+ip
 })
 .then(r=>r.json())
-.then(data=>{
-document.getElementById("results").innerHTML=
-JSON.stringify(data,null,2)
-})
+.then(show)
+
+}
+
+function usernameSearch(){
+
+let u=document.getElementById("username").value
+
+window.open("https://www.google.com/search?q="+u)
+
+}
+
+function phoneSearch(){
+
+let p=document.getElementById("phone").value
+
+window.open("https://www.google.com/search?q="+p)
+
+}
+
+function domainSearch(){
+
+let d=document.getElementById("domain").value
+
+window.open("https://who.is/whois/"+d)
+
+}
+
+function emailSearch(){
+
+let e=document.getElementById("email").value
+
+window.open("https://www.google.com/search?q="+e)
 
 }
