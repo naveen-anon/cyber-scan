f8f5e07f1c2628c993bf843efef4e2227ec0ab5b
Naveen khatri <naveen1khatri@gmail.com>
2026-03-12 15:24:29 +0530

    Add search functionality with SQLite database
    Initialize SQLite database and create searches table. Implement search functionality to store queries and display them on a dashboard.

--- app.py
+++ app.py
@@ -1,1 +1,54 @@
 
+from flask import Flask,render_template,request,redirect
+import sqlite3
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS searches(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        query TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/search",methods=["POST"])
+def search():
+
+    query=request.form["query"]
+
+    conn=sqlite3.connect("database.db")
+    cur=conn.cursor()
+
+    cur.execute("INSERT INTO searches(query) VALUES(?)",(query,))
+    conn.commit()
+    conn.close()
+
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+
+    conn=sqlite3.connect("database.db")
+    cur=conn.cursor()
+
+    cur.execute("SELECT * FROM searches")
+    data=cur.fetchall()
+
+    conn.close()
+
+    return render_template("dashboard.html",data=data)
+
+app.run(debug=True)
