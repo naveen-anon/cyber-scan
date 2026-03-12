0f5fc13fe153363110f59f7e85ba47c515792d7b
Naveen khatri <naveen1khatri@gmail.com>
2026-03-12 17:51:22 +0530

    Add IP search functionality with fetch request

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,18 @@
+function ipSearch(){
 
+let ip=document.getElementById("ip").value
+
+fetch("/ip_lookup",{
+method:"POST",
+headers:{
+"Content-Type":"application/x-www-form-urlencoded"
+},
+body:"ip="+ip
+})
+.then(r=>r.json())
+.then(data=>{
+document.getElementById("results").innerHTML=
+JSON.stringify(data,null,2)
+})
+
+}
