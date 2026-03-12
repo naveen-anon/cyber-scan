00b49ddd021f6653320ef8837a68015fe4ea857f
Naveen khatri <naveen1khatri@gmail.com>
2026-03-12 17:36:22 +0530

    Add username lookup for multiple social media sites
    Fix missing commas and add Google and GitLab URLs.

--- modules/username_lookup.py
+++ modules/username_lookup.py
@@ -1,1 +1,15 @@
+def lookup(username):
 
+    sites = [
+
+        f"https://github.com/{username}",
+        f"https://twitter.com/{username}",
+        f"https://instagram.com/{username}"
+        f"https://facebook.com/{username}"
+        f"https://spotify.com/{username}"
+        f"https://google.com/{username}"
+        f"https//gitlab.com/{username}"
+
+    ]
+
+    return sites
