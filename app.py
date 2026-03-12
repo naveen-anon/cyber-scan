09c674fffe0f69801476655eb50605794c68e20d
Naveen khatri <naveen1khatri@gmail.com>
2026-03-12 19:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,59 @@
-from flask import Flask,render_template,request,redirect,session,jsonify
-from database.models import init_db
-from database.db import get_db
-
-from modules.ip_lookup import lookup as ip_lookup
-from modules.domain_lookup import lookup as domain_lookup
-from modules.username_lookup import lookup as username_lookup
+lfrom flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key="osint_secret"
-
-init_db()
 
 @app.route("/")
 def home():
-
-    if "user" in session:
-        return redirect("/dashboard")
-
     return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+
+    user = cur.fetchone()
+
+    conn.close()
+
+    if user:
+        return redirect("/dashboard")
+    else:
+        return "Login Failed"
+
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users(username,password) VALUES(?,?)",(username,password))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
 
 
 @app.route("/dashboard")
 def dashboard():
-
-    if "user" not in session:
-        return redirect("/")
-
     return render_template("dashboard.html")
-
-
-@app.route("/ip_lookup",methods=["POST"])
-def ip_tool():
-
-    ip=request.form["ip"]
-
-    data=ip_lookup(ip)
-
-    return jsonify(data)
-
-
-@app.route("/domain_lookup",methods=["POST"])
-def domain_tool():
-
-    domain=request.form["domain"]
-
-    data=domain_lookup(domain)
-
-    return jsonify(str(data))
-
-
-@app.route("/username_lookup",methods=["POST"])
-def username_tool():
-
-    username=request.form["username"]
-
-    data=username_lookup(username)
-
-    return jsonify(data)
 
 
 app.run(debug=True)
